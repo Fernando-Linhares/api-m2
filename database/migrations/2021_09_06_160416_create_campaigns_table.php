ce8e45000fb81daa3550e0b960d34f30ce2e62bd
Fernando Linhares Silvestre <fernandomadethat@gmail.com>
2021-09-06 14:19:54 -0300

    add tables features

--- database/migrations/2021_09_06_160416_create_campaigns_table.php
+++ database/migrations/2021_09_06_160416_create_campaigns_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateCampaignsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->morphs('tokenable');
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->id();
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->boolean('is_active');
+            $table->boolean('is_shutdown');
+            $table->text('description');
+            $table->string('goal_description');
+            $table->date('start_date');
+            $table->date('end_date');
             $table->timestamps();
         });
     }
@@ -31,6 +33,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('campaigns');
     }
 }
